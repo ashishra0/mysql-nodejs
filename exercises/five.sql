0e203d9fd4c1c480703849e806a306ef4c59f7e8
Ashish Rao <ashishrao2598@gmail.com>
2019-09-10 12:00:13 +0530

    add exercise five

---
+++ exercises/five.sql
@@ -0,0 +1,12 @@
+-- Calculate total number of users for each email host
+
+SELECT CASE 
+         WHEN email LIKE '%@gmail.com' THEN 'gmail' 
+         WHEN email LIKE '%@yahoo.com' THEN 'yahoo' 
+         WHEN email LIKE '%@hotmail.com' THEN 'hotmail' 
+         ELSE 'other' 
+       end      AS provider, 
+       Count(*) AS total_users 
+FROM   users 
+GROUP  BY provider 
+ORDER  BY total_users;
